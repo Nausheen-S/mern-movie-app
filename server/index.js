569350b8405602d58f2d09ecd6dfa0970e66495e
Nausheen_S <salauddinnausheen@gmail.com>
2021-01-14 15:39:38 +0800

    added mern app

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const db = require('./db')
+const movieRouter = require('./routes/movie-router')
+
+const app = express()
+const apiPort = process.env.PORT || 3000
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+app.use(bodyParser.json())
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.use('/api', movieRouter)
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('./client/build'));
+    app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+
+app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
